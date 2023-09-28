fd1f7d02a37d8eb5ba448add5cc06cb93ff8cd6b
Zenha <zenha.solorzano.acevedo@gmail.com>
2023-09-28 10:43:59 -0300

    Chamada ao chat gpt
    Chamada à API do char GPT com pergunta provisoria.  Instalacao de axios

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,1 +1,26 @@
-// API_KEY="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"
+
+const axios = require('axios');
+
+const apiKey = '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'
+const apiUrl = 'https://api.openai.com/v1/completions'
+
+const prompt = 'Qual a taxonomia de baleia';
+
+// gpt-3.5-turbo
+// text-davinci-003
+
+axios.post(apiUrl, {
+  model: 'text-davinci-003',
+  prompt,
+  temperature: 0.5,
+  max_tokens: 4000
+}, {
+  headers: {
+    'Content-Type' : 'application/json',
+    'Authorization' : `Bearer ${apiKey}`
+  }
+})
+.then(response => {
+  console.log(response.data.choices[0].text)
+})
+.catch(error => console.log(error));
